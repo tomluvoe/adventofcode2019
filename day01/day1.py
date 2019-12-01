58ba186c5a52321fd4979d046f84c9783c0f23d8
Thomas Luvoe <tom@samoht.se>
2019-12-01 21:16:53 +0100

    Updated

--- day01/day1.py
+++ day01/day1.py
@@ -4,26 +4,31 @@ input_file = "input"
 data = ""
 
 with open(input_file, 'r') as input_file:
-    data = input_file.readlines()
+    data = input_file.read()[:-1]
 
-def fuel(mass):
-    return int(mass)//3-2
-
-def fuel_r(mass):
-    if fuel(mass) > 0:
-        return fuel(mass) + fuel_r(fuel(mass))
-    return 0
-
-def fuel_sum(mass):
-    sum = 0
-    for i in range(len(mass)):
-        sum += fuel(mass[i])
+def fuel(m, r = False):
+    if r:
+        sum = 0
+        x = m//3-2
+        while x > 0:
+            sum += x
+            x = x//3-2
+    else:
+        sum = m//3-2
     return sum
 
-def fuel_sum_r(mass):
+def part1(data):
+    mass = map(int, data.split('\n'))
     sum = 0
-    for i in range(len(mass)):
-        sum += fuel_r(mass[i])
+    for m in mass:
+        sum += fuel(m)
+    return sum
+
+def part2(data):
+    mass = map(int, data.split('\n'))
+    sum = 0
+    for m in mass:
+        sum += fuel(m, True)
     return sum
 
 def unit_test_p1():
@@ -39,19 +44,19 @@ def unit_test_p1():
 
 def unit_test_p2():
     print("Unit test start:")
-    assert fuel_r(14) == 2
+    assert fuel(14, True) == 2
     print("Test 1 OK")
-    assert fuel_r(1969) == 966
+    assert fuel(1969, True) == 966
     print("Test 2 OK")
-    assert fuel_r(100756) == 50346
+    assert fuel(100756, True) == 50346
     print("Test 3 OK")
 
 print("My data:\n", data, "\n")
 
 print("** Part one")
 unit_test_p1()
-print("My solution is: ", fuel_sum(data), "\n")
+print("My solution is: ", part1(data), "\n")
 
 print("** Part two")
 unit_test_p2()
-print("My solution is: ", fuel_sum_r(data), "\n")
+print("My solution is: ", part2(data), "\n")
