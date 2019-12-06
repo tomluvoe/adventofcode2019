f233c672564f682e0ce3fc1bc71ba501bea35247
Thomas Luvoe <tom@samoht.se>
2019-12-06 15:56:57 +0100

    day5

--- day05/day05.py
+++ day05/day05.py
@@ -17,11 +17,10 @@ def step(dat,pc=0,input=0):
     if len(str(instr)) > 2:
         instr = int(str(dat[pc])[-2:])
         pm = '00'+str(dat[pc])[:-2]
-    if instr < 3 or instr == 4 or instr > 6:
-        if pm[-1] == '0':
-            p1 = dat[dat[pc+1]]
-        else:
-            p1 = dat[pc+1]
+    if pm[-1] == '0':
+        p1 = dat[dat[pc+1]]
+    else:
+        p1 = dat[pc+1]
     if instr < 3 or instr > 4:
         if pm[-2] == '0':
             p2 = dat[dat[pc+2]]
@@ -37,12 +36,12 @@ def step(dat,pc=0,input=0):
         print(p1)
         pc += 2
     elif(instr == 5):
-        if dat[pc+1] != 0:
-            pc = p2
-        else:
+        if p1 == 0:
             pc += 3
+        else:
+            pc = p2
     elif(instr == 6):
-        if dat[pc+1] == 0:
+        if p1 == 0:
             pc = p2
         else:
             pc += 3
@@ -65,7 +64,6 @@ def part1(data, input=0):
     pc = 0
     while dat[pc] != 99:
         dat,pc = step(dat,pc,input)
-        print(pc)
     return dat
 
 def unit_test_p1():
@@ -75,12 +73,22 @@ def unit_test_p1():
     assert part1("1101,100,-1,4,0") == [1101,100,-1,4,99]
     print("Test 2 OK")
 
+def unit_test_p2():
+    print("Unit test start;")
+    dat = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99"
+    assert part1(dat,1)[0]
+    print("Test 1 OK if 999 output")
+    assert part1(dat,8)[0]
+    print("Test 2 OK if 1000 output")
+    assert part1(dat,9)[0]
+    print("Test 3 OK if 1001 output")
 
 print("My data:\n", data, "\n")
 
 print("** Part one")
 unit_test_p1()
-print("My solution is: ", part1(data,1)[0], "\n")
+part1(data,1)
 
 print("** Part two")
-print("My solution is: ", part1(data,5)[0], "\n")
+unit_test_p2()
+part1(data,5)
